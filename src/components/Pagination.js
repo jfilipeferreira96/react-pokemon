914a58d7a3a191b2fb7afcc1fc2b8c1734562758
Filipe Ferreira <jfilipeferreira@ua.pt>
2022-05-03 23:01:57 +0100

    new pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,10 +5,10 @@ export default function Pagination({page, totalPages, onLeftClick, onRightClick}
     return (
         <nav className="pagination is-centered" role="navigation" aria-label="pagination">
             <button className="pagination-previous" onClick={onLeftClick}>Previous</button>
-            <button className="pagination-next is-disabled" onClick={onRightClick}>Next page</button>
             <ul className="pagination-list">
                 <li><span className="pagination-link">{page} / {totalPages}</span></li>
             </ul>
+            <button className="pagination-next is-disabled" onClick={onRightClick}>Next page</button>
         </nav>
     )
 }
